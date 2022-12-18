51fbb3cc7a6a706f36e63e1eacdde40f656fedc4
DevExpressExampleBot <fakeEmail>
2022-12-18 19:49:32 +0400

    Source auto update [skip ci]

--- VB/ValuesOfAutomaticDateTimeScale/MainWindow.xaml.vb
+++ VB/ValuesOfAutomaticDateTimeScale/MainWindow.xaml.vb
@@ -1,32 +1,24 @@
-﻿Imports Microsoft.VisualBasic
 Imports DevExpress.Xpf.Charts
-Imports System
 Imports System.Windows
 
 Namespace ValuesOfDateTimeScale
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub ChartControl_AxisScaleChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Charts.AxisScaleChangedEventArgs)
-			Dim axisX2D As AxisX2D = TryCast(e.Axis, AxisX2D)
-			If axisX2D Is Nothing Then
-				Return
-			End If
-			Dim options As New ContinuousDateTimeScaleOptions()
-			If options Is Nothing Then
-				Return
-			End If
-			axisX2D.Title = New AxisTitle With {
-				.Visible = True,
-				.Content = String.Format("The Axis Grid Alignment Unit is {0}" & vbCrLf & "The Axis Grid Spacing is {1}", options.GridAlignment, options.GridSpacing)
-			}
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub ChartControl_AxisScaleChanged(ByVal sender As Object, ByVal e As AxisScaleChangedEventArgs)
+            Dim axisX2D As AxisX2D = TryCast(e.Axis, AxisX2D)
+            If axisX2D Is Nothing Then Return
+            Dim options As ContinuousDateTimeScaleOptions = New ContinuousDateTimeScaleOptions()
+            If options Is Nothing Then Return
+            axisX2D.Title = New AxisTitle With {.Visible = True, .Content = String.Format("The Axis Grid Alignment Unit is {0}" & Microsoft.VisualBasic.Constants.vbCrLf & "The Axis Grid Spacing is {1}", options.GridAlignment, options.GridSpacing)}
+        End Sub
+    End Class
 End Namespace
